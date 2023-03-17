b753ef18505d47a38b6da3df3b3aeb2efad79c7d
Jay Fukumoto <jayfukum@gmail.com>
2023-03-17 15:10:59 -0700

    Initial Commit

---
+++ Zzzot/ZzzotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZzzotApp.swift
+//  Zzzot
+//
+//  Created by Jay Fukumoto on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ZzzotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
